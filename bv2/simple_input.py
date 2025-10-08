c50e01ce41cadf6e9ce7e97e1f75903881a4dac1
Alexander Kolesnikov <alexanderkolesnikoff@gmail.com>
2025-10-08 12:28:59 +0000

    Update obsolete `dont_repeat` default. Our `state_after` field is currently repeated.

--- bv2/simple_input.py
+++ bv2/simple_input.py
@@ -36,7 +36,7 @@ def iter_packed_examples(
     *,
     leader="tokens",
     debugid="id",
-    dont_repeat=("id", "rng_state_before"),
+    dont_repeat=("id", "state_after"),
 ):
     """Numpy arrays get packed, anything else gets repeated. Add `iseq` counter."""
 
