32966d72fb7c9cf1b283ba50717789b034da2fc6
lucasb-eyer <lucasb.eyer.be@gmail.com>
2025-10-27 13:32:39 +0100

    Make tokenizer treat special tokens as raw text. Needed for code, and it's the only thing that makes sense, really.

--- bv2/data/tokenizer.py
+++ bv2/data/tokenizer.py
@@ -27,7 +27,7 @@ class Tiktoken:
         self.bos = base_dict_size
         self.eos = base_dict_size + 1
         self.sep = base_dict_size + 2
-        special_tokens = {
+        self.special_tokens = {
             "<|bos|>": self.bos,
             "<|eos|>": self.eos,
             "<|sep|>": self.sep,
@@ -37,13 +37,17 @@ class Tiktoken:
             name="l4_200k_base",
             pat_str=O200K_PATTERN,
             mergeable_ranks=tokens,
-            special_tokens=special_tokens,
+            special_tokens=self.special_tokens,
         )
 
         self.n_vocab = self.tokenizer.n_vocab
 
     def encode(self, text):
-        return self.tokenizer.encode(text)
+        # This setting means all special tokens are encoded as plain text and not
+        # treated specially. They also do not raise an exception - we don't want
+        # to kill jobs far into training. Maybe we should print/log a warning though?
+        # To use special tokens, just use tt.special_tokens["<|eos|>"] or tt.eos.
+        return self.tokenizer.encode(text, disallowed_special=())
 
     def decode(self, text):
         return self.tokenizer.decode(text)
