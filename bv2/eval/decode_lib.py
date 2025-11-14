0c3cd1aeea915dfc0d589552e751ce68094f2b02
Lucas Beyer <qkv@meta.com>
2025-11-14 22:05:44 +0100

    We dropped a -1

--- bv2/eval/decode_lib.py
+++ bv2/eval/decode_lib.py
@@ -75,7 +75,7 @@ def decode_batch(predict_fn, batch, *, decode_idx, rng,
         if all(u.all_gather_object(all(reached_eos))):
             break
 
-        logits_tok_idx = torch.from_numpy(decode_idx).to(device)
+        logits_tok_idx = torch.from_numpy(decode_idx - 1).to(device)
         logits = predict_fn(tokens, flex_masks, None, torch.zeros(tokens.shape[:2], dtype=torch.int64), mode="logits", logits_tok_idx=logits_tok_idx)
 
         # TODO: add support for T=0
